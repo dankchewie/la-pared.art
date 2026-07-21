e1fa4d50c43bed3861a05822b7f5bf569ced2c8d
Kai <kaiaescobar@gmail.com>
2026-07-21 16:58:08 -0600

    Redirect kaiateycrea.com to lapared.xyz/kaiateycrea

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [{ type: "host", value: "kaiateycrea.com" }],
+        destination: "https://lapared.xyz/kaiateycrea",
+        permanent: true,
+      },
+      {
+        source: "/:path*",
+        has: [{ type: "host", value: "www.kaiateycrea.com" }],
+        destination: "https://lapared.xyz/kaiateycrea",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
